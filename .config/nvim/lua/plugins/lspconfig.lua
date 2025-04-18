15d0f86ba907d8f835ab3c9d95b10d5c593ee513
Marc Deop i Argemí <marc@marcdeop.com>
2025-04-18 11:53:50 +0200

    fix: disable hover on ruff as we use pyright

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -69,6 +69,20 @@ require('lspconfig')['ts_ls'].setup{
 }
 
 require('lspconfig')['ruff'].setup{}
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then
+      return
+    end
+    if client.name == 'ruff' then
+      -- Disable hover in favor of Pyright
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+  desc = 'LSP: Disable hover capability from Ruff',
+})
 
 require('lspconfig')['rust_analyzer'].setup{
   capabilities = capabilities,
